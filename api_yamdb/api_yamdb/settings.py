b93fd3aa7ee83558e233b86c328d123953e28d85
NikLukyan <nikluk@mail.ru>
2023-04-08 15:14:29 +0300

    API_Yamdb project in Docker containers ver 3.0

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', default=None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
